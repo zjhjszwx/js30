def7b2a82b88455e6d434f193d45a7b97110be9e
yc <oncwnuOGiXjFFTofRKIylRZ0ys1o@git.weixin.qq.com>
2020-11-15 22:13:29 +0800

    js02

--- 02 - JS + CSS Clock/index.js
+++ 02 - JS + CSS Clock/index.js
@@ -1,22 +1,36 @@
 var arr = [1, 2, 1, 1, 1, 2, 3]
 // 2次循环
-function func(arr) {
-  let res = [];
-  for (let i = 0; i < arr.length; i++) {
-    //是否插入 res
-    let flag = true
-    for (let j = 0; j < res.length; j++) {
-      const element = res[j];
-      if (arr[i] == element) {
-        flag = false
-        break;
-      }
-    }
-    if (flag) {
-      res.push(arr[i])
-    }
-  }
-  return res
-}
+// function func(arr) {
+//   let res = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     //是否插入 res
+//     let flag = true
+//     for (let j = 0; j < res.length; j++) {
+//       const element = res[j];
+//       if (arr[i] == element) {
+//         flag = false
+//         break;
+//       }
+//     }
+//     if (flag) {
+//       res.push(arr[i])
+//     }
+//   }
+//   return res
+// }
 
-console.log(func(arr))
+//使用indexof
+//判断元素是否在数组中, 返回元素的位置
+// function func(array) {
+//   let res = []
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     if (res.indexOf(element) > -1) {
+//       continue;
+//     }
+//     res.push(element)
+//   }
+//   return res
+// }
+
+// console.log(func(arr))
