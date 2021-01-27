11d4fb8d3be8aa6e899ee59ba35651345a3f2f99
zhangwenxin <17830723072@dingtalk.com>
2021-01-27 17:49:09 +0800

    1

---
+++ 06 - Type Ahead/index.js
@@ -0,0 +1,18 @@
+// JavaScript专题之如何判断两个对象相等
+
+/*
+我们认为：
+
+1.NaN 和 NaN 是相等
+2.[1] 和 [1] 是相等
+3.{value: 1} 和 {value: 1} 是相等
+不仅仅是这些长得一样的，还有
+
+1 和 new Number(1) 是相等
+2.'Curly' 和 new String('Curly') 是相等
+3.true 和 new Boolean(true) 是相等
+*/
+
+function eq(a, b) {
+  
+}
